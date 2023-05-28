5497146880e55bee3bb6bbffd9b44580ff7d71e5
LexHeadko <n-odes11rus@yandex.ru>
2023-05-28 16:26:51 +0200

    branch commit

--- oop25-oop25/oop25-oop25/src/main/java/com/skypro/ex/oop25/model/Employee.java
+++ oop25-oop25/oop25-oop25/src/main/java/com/skypro/ex/oop25/model/Employee.java
@@ -39,7 +39,7 @@ public class Employee {
 
     @Override
     public String toString() {
-        return  "Работник{" +
+        return  "Работник {" +
                 "Имя = " + firstName + " " +
                 "Фамилия = " + lastName +
                 "}";
